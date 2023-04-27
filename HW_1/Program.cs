9f2c560d07c549ffd6ff5ff940b4136ab433f10a
Nikolay_aka_Duke <w-n-a@ya.ru>
2023-04-27 18:55:12 +0300

    Task8 well done

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -48,14 +48,31 @@
 
 // Task 6. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
-Console.WriteLine("Input number:");
-int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input number:");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-if (num % 2 == 0)
+// if (num % 2 == 0)
+// {
+//     Console.WriteLine("You entered an even number.");
+// }
+// else
+// {
+//     Console.WriteLine("You entered an odd number.");
+// }
+
+
+// Task 8. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Input N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+while (count <= n)
 {
-    Console.WriteLine("You entered an even number.");
-}
-else
-{
-    Console.WriteLine("You entered an odd number.");
+    if (count % 2 == 0)
+    {
+        Console.Write(count+", ");
+    }
+    count++;
 }
