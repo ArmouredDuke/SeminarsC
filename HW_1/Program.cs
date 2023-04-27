6592c97b9dfa14b37d6ba9b5e87812fcf9b30574
Nikolay_aka_Duke <w-n-a@ya.ru>
2023-04-27 18:18:18 +0300

    Task4 well done, но есть вопросы к Архипу!=)

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -1,23 +1,48 @@
 ﻿// Task 2.  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-Console.WriteLine("Input first number:");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input second number:");
-int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input first number:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number:");
+// int num2 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 > num2)
-{
-    Console.WriteLine($"The number {num1} is greater than the number {num2}.");
-}
-if (num1 < num2)
-{
-    Console.WriteLine($"The number {num1} is smoller than the number {num2}.");
-}
-if (num1 == num2)
-{    
-    Console.WriteLine($"The number {num1} is equal to the number {num2}.");
-}
+// if (num1 > num2)
+// {
+//     Console.WriteLine($"The number {num1} is greater than the number {num2}.");
+// }
+// if (num1 < num2)
+// {
+//     Console.WriteLine($"The number {num1} is smoller than the number {num2}.");
+// }
+// if (num1 == num2)  // Тоже возможный вариант
+// {    
+//     Console.WriteLine($"The number {num1} is equal to the number {num2}.");
+// }
 
 
 
-// Task 4. 
+// Task 4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.WriteLine("Input first number:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Input second number:");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Input third number:");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// int max = num1;
+
+// if (num2 > max)
+// {
+//     max = num2;
+// }
+
+// if (num3 > max)
+// {
+//     max = num3;
+// }
+
+// Console.WriteLine(max);
+
+
