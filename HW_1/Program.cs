a066f9d58d4dd01177f4e109d373d4b73064cc40
Nikolay_aka_Duke <w-n-a@ya.ru>
2023-04-27 19:39:51 +0300

    Test

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -63,16 +63,16 @@
 
 // Task 8. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
-Console.WriteLine("Input N:");
-int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input N:");
+// int n = Convert.ToInt32(Console.ReadLine());
 
-int count = 1;
+// int count = 1;
 
-while (count <= n)
-{
-    if (count % 2 == 0)
-    {
-        Console.Write(count+", ");
-    }
-    count++;
-}
+// while (count <= n)
+// {
+//     if (count % 2 == 0)
+//     {
+//         Console.Write(count+", ");
+//     }
+//     count++;
+// }
