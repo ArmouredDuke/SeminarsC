9b3b089b23433fd78ec0daa6d68fda767fab66e4
Nikolay_aka_Duke <w-n-a@ya.ru>
2023-05-29 17:03:57 +0300

    CopyPaste well done

--- HW_2/Program.cs
+++ HW_2/Program.cs
@@ -1,59 +1,59 @@
 ﻿// Task 10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
-int Prompt(string message)
-{
-    System.Console.Write(message);
-    string value = Console.ReadLine();
-    int result = Convert.ToInt32(value);
-    return result;
-}
+// int Prompt(string message)
+// {
+//     System.Console.Write(message);
+//     string value = Console.ReadLine();
+//     int result = Convert.ToInt32(value);
+//     return result;
+// }
 
-int number = Prompt("Введите трёхзначное число -> ");
-if (number < 100 || number >= 1000) // Внимание, вопрос: а что будем делать, если число отрицательное?
-{
-    Console.WriteLine("Вы ввели не трёхзначное число. Пожалуйста, повторите ввод.");
-    return;
-}
+// int number = Prompt("Введите трёхзначное число -> ");
+// if (number < 100 || number >= 1000) // Внимание, вопрос: а что будем делать, если число отрицательное?
+// {
+//     Console.WriteLine("Вы ввели не трёхзначное число. Пожалуйста, повторите ввод.");
+//     return;
+// }
 
-Console.WriteLine($"Вы ввели число '{number}'");
-int secondRank = number / 10 % 10;
-Console.WriteLine($"Вторая цифра данного числа - '{secondRank}'");
+// Console.WriteLine($"Вы ввели число '{number}'");
+// int secondRank = number / 10 % 10;
+// Console.WriteLine($"Вторая цифра данного числа - '{secondRank}'");
 
 
-// Task 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// // Task 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-int Prompt(string message)
-{
-    Console.Write(message);
-    string value = Console.ReadLine();
-    int result = Convert.ToInt32(value);
-    return result;
-}
+// int Prompt(string message)
+// {
+//     Console.Write(message);
+//     string value = Console.ReadLine();
+//     int result = Convert.ToInt32(value);
+//     return result;
+// }
 
-int GetThirdRank(int number)
-{
-    while (number > 999)
-    {
-        number /= 10;
-    }
-    return number % 10;
-}
+// int GetThirdRank(int number)
+// {
+//     while (number > 999)
+//     {
+//         number /= 10;
+//     }
+//     return number % 10;
+// }
 
-bool ValidateNumber(int number)
-{
-    if (number < 100)
-    {
-        Console.WriteLine("Третьей цифры нет.");
-        return false;
-    }
-    return true;
-}
+// bool ValidateNumber(int number)
+// {
+//     if (number < 100)
+//     {
+//         Console.WriteLine("Третьей цифры нет.");
+//         return false;
+//     }
+//     return true;
+// }
 
-int number = Prompt("Введите число -> ");
-if (ValidateNumber(number))
-{
-    Console.WriteLine(GetThirdRank(number));
-}
+// int number = Prompt("Введите число -> ");
+// if (ValidateNumber(number))
+// {
+//     Console.WriteLine(GetThirdRank(number));
+// }
 
 
 // Task 15. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
