5a4d61e474ad497554f92363b7c0779b1474b51a
Nikolay_aka_Duke <w-n-a@ya.ru>
2023-05-31 20:28:23 +0300

    HW6.T2

--- HW_6/Program.cs
+++ HW_6/Program.cs
@@ -1,51 +1,113 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-int Prompt(string message)
+// int Prompt(string message)
+// {
+//     System.Console.Write(message);
+//     string value = Console.ReadLine();
+//     int resalt = Convert.ToInt32(value);
+//     return resalt;
+// }
+
+// int[] InputArray(int lenght)
+// {
+//     int[] arrey = new int[lenght];
+//     for (int i = 0; i < arrey.Lenght; i++)
+//     {
+//         arrey[i] = Prompt($"Введите {i + 1} элемент.");
+//     }
+//     return arrey;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.WriteLine($"a[{i}] = {array[i]}");
+//     }
+// }
+
+// int CountPositiveNumbers(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > 0)
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// int lenght = Prompt("Введите количество элементов: ");
+// int[] arrey;
+// arrey = InputArray(lenght);
+// PrintArray(arrey);
+// Console.WriteLine($"Количество чисел больше нуля - {CountPositiveNumbers(arrey)}");
+
+
+
+
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+const int COEFFICIENT = 0;
+const int CONSTANT = 1;
+const int X_COORD = 0;
+const int Y_COORD = 1;
+const int LINE1 = 1;
+const int LINE2 = 2;
+
+double[] lineData1 = InputLineData(LINE1);
+double[] lineData2 = InputLineData(LINE2);
+
+if (ValidateLines(lineData1, lineData2))
+{
+    double[] coord = FindCoords(lineData1, lineData2);
+    Console.Write($"Точка пересечения уравнений y={lineData1[COEFFICIENT]}*x+{lineData1[CONSTANT]} и y={lineData2[COEFFICIENT]}*x+{lineData2[CONSTANT]} ");
+    Console.WriteLine($" имеет координаты ({coord[X_COORD]}, {coord[Y_COORD]}) ");
+}
+
+double Prompt(string message)
 {
     System.Console.Write(message);
     string value = Console.ReadLine();
-    int resalt = Convert.ToInt32(value);
+    double resalt = Convert.ToDouble(value);
     return resalt;
 }
 
-int[] InputArray(int lenght)
+double[] InputLineData(int numberOfLine)
 {
-    int[] arrey = new int[lenght];
-    for (int i = 0; i < arrey.Lenght; i++)
-    {
-        arrey[i] = Prompt($"Введите {i + 1} элемент.");
-    }
-    return arrey;
+    double[] lineData = new double[2];
+    lineData[COEFFICIENT] = Prompt($"Введите коэффициент для {numberOfLine} прямой >");
+    lineData[CONSTANT] = Prompt($"Введите константу для {numberOfLine} прямой >");
+    return lineData;
 }
 
-void PrintArray(int[] array)
+double[] FindCoords(double[] lineData1, double[] lineData2)
 {
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.WriteLine($"a[{i}] = {array[i]}");
-    }
+    double[] coord = new double[2];
+    coord[X_COORD] = (lineData1[CONSTANT] - lineData2[CONSTANT]) / (lineData2[COEFFICIENT] - lineData1[COEFFICIENT]);
+    coord[Y_COORD] = lineData1[CONSTANT] * coord[X_COORD] + lineData1[CONSTANT];
+    return coord;
 }
 
-int CountPositiveNumbers(int[] array)
+bool ValidateLines(double[] lineData1, double[] lineData2)
 {
-    int count = 0;
-    for (int i = 0; i < array.Length; i++)
+    if (lineData1[COEFFICIENT] == lineData2[COEFFICIENT])
     {
-        if (array[i] > 0)
+        if (lineData1[CONSTANT] == lineData2[CONSTANT])
         {
-            count++;
+            Console.WriteLine("Прямые совпадают!");
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("Прямые параллельны!");
+            return false;
         }
     }
-    return count;
+    return true;
 }
-
-int lenght = Prompt("Введите количество элементов: ");
-int[] arrey;
-arrey = InputArray(lenght);
-PrintArray(arrey);
-Console.WriteLine($"Количество чисел больше нуля - {CountPositiveNumbers(arrey)}");
-
-
-
-
-
