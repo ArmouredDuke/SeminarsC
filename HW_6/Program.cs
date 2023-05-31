7941c70521bd70fb96e7d027aa161437af582791
Nikolay_aka_Duke <w-n-a@ya.ru>
2023-05-31 18:49:28 +0300

    HW6,T1

---
+++ HW_6/Program.cs
@@ -0,0 +1,51 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int resalt = Convert.ToInt32(value);
+    return resalt;
+}
+
+int[] InputArray(int lenght)
+{
+    int arrey = new int[lenght];
+    for (int i = 0; i < arrey.lenght; i++)
+    {
+        arrey[i] = Prompt($"Введите {i + 1} элемент.");
+    }
+    return arrey;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"a[{i}] = {array[i]}");
+    }
+}
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int lenght = Prompt("Введите количество элементов: ");
+int[] arrey;
+arrey = InputArray(lenght);
+PrintArray(arrey);
+Console.WriteLine($"Количество чисел больше нуля - {CountPositiveNumbers(arrey)}");
+
+
+
+
