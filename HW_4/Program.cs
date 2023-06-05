b0e97231800827f876b51f9532d6eed8dda27870
Nikolay_aka_Duke <w-n-a@ya.ru>
2023-06-05 19:32:00 +0300

    HW_4 done

--- HW_4/Program.cs
+++ HW_4/Program.cs
@@ -1,39 +1,74 @@
-﻿// Task 25. написать цикл, принимающий на вход числа A и B, и возводящий A в степень B.
+﻿﻿// ЗАДАЧА 1 ->  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-// int Prompt(string message)
+
+// double Pows(int a, int b)
 // {
-//     System.Console.WriteLine(message);
-//     string readInput = System.Console.ReadLine();
-//     int result = int.Parse(readInput);
+//     return Math.Pow(a, b);
+// }
+
+// Console.WriteLine("Введите число А");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число В");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"Число {a} в степени {b} -> {Pows(a,b)}");
+
+
+
+
+
+// ЗАДАЧА 2 -> Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// int Sum(int num)
+// {
+//     int result = 0;
+//     while(num > 0)
+//     {
+//         int last = num % 10;
+//         result = result + last;
+//         num /= 10;
+//     }
+//     result = result + num;
 //     return result;
 // }
 
-// int Power(int powerBase, int exponent)
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"Сумма цифр в числе {num} -> {Sum(num)}");
+
+
+// ЗАДАЧА 3 -> Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// int[] MyArray (int size, int min, int max)
 // {
-//     int power = 1;
-//     for (int i = 0; i < exponent; i++)
+//     int[] array = new int[size];
+//     for(int i = 0; i < size; i++)
 //     {
-//         power *= powerBase;
+//         array[i] = new Random().Next(min, max + 1);
 //     }
-//     return power;
-// }
+//     return array;
+// } 
 
-// bool ValidateExponent(int exponent)
+// void PrintArray(int[] array)
 // {
-//     if (exponent < 0)
+//     for(int i = 0; i < array.Length; i++)
 //     {
-//         System.Console.WriteLine("Показатель не должен быть меньше нуля.");
-//         return false;
+//         Console.Write($"{array[i]} ");
 //     }
-//     return true;
+//     Console.WriteLine();
 // }
 
-// int powerBase = Prompt("Введите основание: ");
-// int exponent = Prompt("Введите показатель: ");
-// if (ValidateExponent(exponent))
-// {
-//     System.Console.WriteLine($"Число {powerBase} в степени {exponent} равно {Power(powerBase, exponent)}");
-// }
+// Console.WriteLine("Введите размер массива");
+// int size =Convert.ToInt32(Console.ReadLine());
 
+// Console.WriteLine("Введите минимально допустимое число в массиве");
+// int min =Convert.ToInt32(Console.ReadLine());
 
+// Console.WriteLine("Введите максимально допустимое число в массиве");
+// int max =Convert.ToInt32(Console.ReadLine());
+
+// int[] newArray = MyArray(size, min, max);
+// Console.Write("Полученный массив -> ");
+// PrintArray(newArray);
 
