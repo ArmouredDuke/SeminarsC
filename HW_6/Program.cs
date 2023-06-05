f3f9669cca908057d13e1cbae45e48ba5a9a1f52
Nikolay_aka_Duke <w-n-a@ya.ru>
2023-06-05 19:23:31 +0300

    HW_5 done

--- HW_6/Program.cs
+++ HW_6/Program.cs
@@ -1,49 +1,49 @@
 ﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-// int Prompt(string message)
-// {
-//     System.Console.Write(message);
-//     string value = Console.ReadLine();
-//     int resalt = Convert.ToInt32(value);
-//     return resalt;
-// }
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int resalt = Convert.ToInt32(value);
+    return resalt;
+}
 
-// int[] InputArray(int lenght)
-// {
-//     int[] arrey = new int[lenght];
-//     for (int i = 0; i < arrey.Lenght; i++)
-//     {
-//         arrey[i] = Prompt($"Введите {i + 1} элемент.");
-//     }
-//     return arrey;
-// }
+int[] InputArray(int lenght)
+{
+    int[] arrey = new int[lenght];
+    for (int i = 0; i < arrey.Lenght; i++)
+    {
+        arrey[i] = Prompt($"Введите {i + 1} элемент.");
+    }
+    return arrey;
+}
 
-// void PrintArray(int[] array)
-// {
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         Console.WriteLine($"a[{i}] = {array[i]}");
-//     }
-// }
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"a[{i}] = {array[i]}");
+    }
+}
 
-// int CountPositiveNumbers(int[] array)
-// {
-//     int count = 0;
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         if (array[i] > 0)
-//         {
-//             count++;
-//         }
-//     }
-//     return count;
-// }
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
 
-// int lenght = Prompt("Введите количество элементов: ");
-// int[] arrey;
-// arrey = InputArray(lenght);
-// PrintArray(arrey);
-// Console.WriteLine($"Количество чисел больше нуля - {CountPositiveNumbers(arrey)}");
+int lenght = Prompt("Введите количество элементов: ");
+int[] arrey;
+arrey = InputArray(lenght);
+PrintArray(arrey);
+Console.WriteLine($"Количество чисел больше нуля - {CountPositiveNumbers(arrey)}");
 
 
 
